1cd24668291cb2263bb83b9e44e9e9fabec1f789
Alyssa Ferry <alyssa.ferry96@gmail.com>
2021-12-16 11:22:30 -0800

    Starting code to get hourly tasks saved to local storage.

--- Develop/script.js
+++ Develop/script.js
@@ -31,15 +31,29 @@ for(var i=8; i<=17; i++){
         else{
             blockTense="future";
         }
-
-
+    var savedValueForHour = localStorage.getItem("hour-"+i);
+    if(!savedValueForHour){
+        savedValueForHour="";
+    }
     var timeblockEl = `<div class="row time-block">
     <div class="col-md-1 hour">${hourData}</div>
-    <textarea class="col-md-10 description ${blockTense}""></textarea>
+    <textarea class="col-md-10 description ${blockTense}" id="hour-${i}">${savedValueForHour}</textarea>
     <button class="btn saveBtn col-md-1">save</button></div>`
     
     container.append(timeblockEl);
+    
+
 }; 
+
+var newTask;
+
+container.on("click", "button", function(event){
+    var taskInput = $(this).siblings(".textarea").attr("id");
+    console.log(taskInput);
+    newTask = $(this).siblings(".textarea").val();
+    localStorage.setItem(taskInput, newTask);
+    console.log(newTask)
+});
 /*
     //save current hour to data attribute to be accessed from event listener
     //
