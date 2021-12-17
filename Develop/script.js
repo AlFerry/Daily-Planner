ef4219410bb7b6992dd34300389465f9600d5159
Alyssa Ferry <alyssa.ferry96@gmail.com>
2021-12-16 20:36:04 -0800

    Added comments to script and moved index.html to main folder and fixed relative paths.

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,4 @@
+
 var today= moment();
 var hour=today.format("H");
 var container = $('.container');
@@ -6,8 +7,9 @@ $('#currentDay').text(today.format("dddd, MMMM Do"));
 console.log(hour);
 
 
-//for loop to loop through hours in work day (8am to 5pm in military time)
+//for loop to loop through hours in work day (8am to 5pm in military time) to create timeblocks
 for(var i=8; i<=17; i++){
+    //Format hour displayed to be in standard time
     var hourData;
         if(i<12){
             hourData = i + " AM";
@@ -19,21 +21,25 @@ for(var i=8; i<=17; i++){
             var modTime = i-12;
             hourData = modTime + " PM";
         };
-
+    
+    //Format color of timeblock to indicate if the hour is in the past, present, or future.
     var blockTense;
-        if( i < hour){
-            blockTense="past";
-        } 
-        else if(i == hour){
-            blockTense="present";
-        }
-        else{
-            blockTense="future";
-        }
+    if( i < hour){
+        blockTense="past";
+    } 
+    else if(i == hour){
+        blockTense="present";
+    }
+    else{
+        blockTense="future";
+    }
+
+    //Populate textareas that have tasks saved to them.
     var savedTask = localStorage.getItem("hour-"+i);
     if(!savedTask){
         savedTask="";
     }
+    //Use template literal to define timeblocks to be added to html page.
     var timeblockEl = `<div class="row time-block">
     <div class="col-md-1 hour">${hourData}</div>
     <textarea class="col-md-10 description ${blockTense}" id="hour-${i}">${savedTask}</textarea>
@@ -44,6 +50,7 @@ for(var i=8; i<=17; i++){
 
 }; 
 
+//Save new tasks entered to planner to local storage when save button is clicked.
 var newTask;
 
 container.on("click", "button", function(e){
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Daily-Planner
 A daily planner to keep track of my busy schedule
+
+As a working individual, I want an interactive calendar of my day
+so that I can see what events are in the past, what are upcoming, and save my inputs so I can leave the page and return.
+
+This 
